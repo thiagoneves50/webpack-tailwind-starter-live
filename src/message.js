8c2a41c90c1718584960c4387e83eda021853e09
Thiago Neves <thiago@weberous.com>
2023-04-14 18:24:56 -0300

    first commit

---
+++ src/message.js
@@ -0,0 +1,6 @@
+const message = {
+  id: 1,
+  text: 'Just testing here',
+}
+
+export default message;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    watchFiles: ('./src/**/*.html'),
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [      
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),    
+  ]
+}
